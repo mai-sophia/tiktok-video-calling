de43bab00bf139ef4e4e8a44e0688676f322b616
Sophia Mai <140025356+mai-sophia@users.noreply.github.com>
2024-07-04 11:23:09 -0500

    Debugging and Reformatting UI

--- react-video-calling/src/App.tsx
+++ react-video-calling/src/App.tsx
@@ -1,9 +1,9 @@
-import { CallingState, StreamCall, StreamVideo, StreamVideoClient, useCall, useCallStateHooks, User } from '@stream-io/video-react-sdk';
+import { CallControls, CallingState, ParticipantView, SpeakerLayout, StreamCall, StreamTheme, StreamVideo, StreamVideoClient, StreamVideoParticipant, useCall, useCallStateHooks, User } from '@stream-io/video-react-sdk';
 
 const apiKey = 'mmhfdzb5evj2';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiWnVja3VzcyIsImlzcyI6Imh0dHBzOi8vcHJvbnRvLmdldHN0cmVhbS5pbyIsInN1YiI6InVzZXIvWnVja3VzcyIsImlhdCI6MTcxOTk4MTQ3MywiZXhwIjoxNzIwNTg2Mjc4fQ.YMfyAs3NJSQLs3Ma4_Ofv590x0E2Fp7iD2j03WBYAkU';
 const userId = 'Zuckuss';
-const callId = '7DBZtbM3OSDC';
+const callId = '7DBZtbM3OSDC'; 
 
 // set up the user object
 const user: User = {
@@ -16,6 +16,23 @@ const client = new StreamVideoClient({ apiKey, user, token });
 const call = client.call('default', callId);
 call.join({ create: true });
 
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+      <StreamTheme>
+        <SpeakerLayout participantsBarPosition='bottom' />
+        <CallControls />
+      </StreamTheme>
+    
+  );
+};
+
 export default function App() {
   return (
     <StreamVideo client={client}>
@@ -24,22 +41,51 @@ export default function App() {
       </StreamCall>
     </StreamVideo>
   );
-}
+};
 
-export const MyUILayout = () => {
-  const call = useCall();
-
-  const { useCallCallingState, useParticipantCount } = useCallStateHooks();
-  const callingState = useCallCallingState();
-  const participantCount = useParticipantCount();
-
-  if (callingState !== CallingState.JOINED) {
-    return <div>Loading...</div>;
-  }
-
+export const MyParticipantList = (props: {
+  participants: StreamVideoParticipant[];
+}) => {
+  const { participants } = props;
   return (
-    <div>
-      Call "{call.id}" has {participantCount} participants
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '8px',
+        width: '100vw',
+      }}
+    >
+      {participants.map((participant) => (
+        <div style={{ width: '100%', aspectRatio: '3 / 2' }}>
+          <ParticipantView
+            muteAudio
+            participant={participant}
+            key={participant.sessionId}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const MyFloatingLocalParticipant = (props: {
+  participant?: StreamVideoParticipant;
+}) => {
+  const { participant } = props;
+  return (
+    <div 
+      style={{
+        position: 'absolute',
+        top: '15px',
+        left: '15px',
+        width: '240px',
+        height: '135px',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 10px 3px',
+        borderRadius: '12px',
+      }}
+    >
+      {participant && <ParticipantView muteAudio participant={participant} />}
     </div>
   );
 };
